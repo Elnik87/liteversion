416ff9a1646e5a418363eb025ff5b7264fddf17a
Elnik87 <65997269+Elnik87@users.noreply.github.com>
2021-10-24 11:30:05 +0300

    Поправил ошибки, но пока не работает удаление и изменение новости

--- news/views.py
+++ news/views.py
@@ -13,11 +13,31 @@ def newses():
 @news.route("/<slug>")
 def news_detail(slug):
     news_detail = News.query.filter(News.slug==slug).first()
-    return render_template("news_detail/detail.html", news_detail=news_detail)
+    return render_template("news/news_detail.html", news_detail=news_detail)
 
-@news.route('/<slug>/delete')
-def delete(id):
-    news = News.query.get_or_404(id)
+@news.route('/<slug>/news_update')
+def news_update(slug):
+    news = News.query.filter(News.slug==slug).first()
+    return render_template("news/news_update.html", news=news)
+
+@news.route('/<slug>/news_update_create')
+def news_update_create(slug):
+    news = News.query.get(slug)
+    if request.method == 'POST':
+        news.title = request.form["title"]
+        news.content = request.form["content"]
+        try:
+            db.session.commit()
+            return redirect('')
+        except:
+            return "Ошибка"
+    else:
+        news = News.query.get(slug)
+        return render_template("news/news_update_create.html", news=news)
+
+@news.route('/<slug>/news_delete')
+def news_delete(slug):
+    news = News.query.get_or_404(slug)
     try:
         db.session.delete(news)
         db.session.commit()
@@ -28,25 +48,24 @@ def delete(id):
 
 @news.route('/create', methods=['POST', 'GET'])
 def create():
-
     if request.method == 'POST':
         news_simple = News(title=request.form["title"], content=request.form["content"])
-        # news_simple.title = request.form['title']
-        # news_simple.content = request.form['content']
+        # title = request.form['title']
+        # content = request.form['content']
         try:
             db.session.add(news_simple)
             db.session.commit()
-            return redirect('/news')
+            return redirect('')
         except:
             return "Ошибка"
-
-    return render_template("news/create.html")
+    else:
+        return render_template("news/create.html")
 
 
 @news.route('/klerjflsejtalsef;lasm;oete;rogmzxd;lfkeargj')
 def admin():
     news = News.query.order_by(News.date.desc()).all()
-    return render_template("/news/admin.html", news=news)
+    return render_template("news/admin.html", news=news)
 
 
 @news.route('/login', methods=['POST', 'GET'])
@@ -54,7 +73,6 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        print(username, password)
         if username == "Elnik" and password == "12345":
             return redirect("klerjflsejtalsef;lasm;oete;rogmzxd;lfkeargj")
         else:
